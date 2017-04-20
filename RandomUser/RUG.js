8b0633f884b36ebbdeb182d1e885d12862375783
imottall <timobuitenhuis@gmail.com>
2017-04-20 16:40:46 +0200

    Update RUG.js

--- RandomUser/RUG.js
+++ RandomUser/RUG.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.get('/', function(request, response) {
  response.send('Hello Avans!');
@@ -7,10 +8,30 @@ app.get('/', function(request, response) {
 app.get('/about', function(request, response) {
  response.send('Written by <jouw naam hier invullen>');
 })
+app.get('/json', function(request, response) {
+ response.json({
+ 'some_name': 'Value',
+ 'an_array_of_objects': [
+ {
+ 'another_name': 'Another value',
+ 'a_further_name': "A further value"
+ },
+ {'yet_another_name': 'Yet another value'}
+ ],
+ 'some_boolean': true,
+ 'some_integer': 42,
+ 'array_of_ints': [
+ 2, 3, 5, 7, 11, 13
+ ],
+ 'array_of_strings': [
+ "twee", "drie", "vijf", "zeven"
+ ]
+ })
+})
 app.all('*', function(request, response) {
  response.status(404);
  response.send('404 -Not found');
 })
-app.listen(8080, function() {
- console.log('Server app is listening on port 8080');
+app.listen(port, function() {
+ console.log('Server app is listening on port ' + port);
 })
