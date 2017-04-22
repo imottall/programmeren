f587ce1feeb2dc4411f435933a703fe91febe5f5
imottall <timobuitenhuis@gmail.com>
2017-04-22 15:22:01 +0200

    Update RUG.js

--- RandomUser/RUG.js
+++ RandomUser/RUG.js
@@ -14,7 +14,8 @@ app.get('/json', function(request, response) {
 	switch (rand) {
     case 0:
         response.json({
-        	"thisistheresult" : "heya"
+        	"results":[{"gender":"female","name":{"title":"miss","first":"maudi","last":"den uijl"},"location":{"street":"4550 jan pieterszoon coenstraat","city":"goeree-overflakkee","state":"drenthe","postcode":86022},"email":"maudi.denuijl@example.com","login":{"username":"yellowlion527","password":"smiles","salt":"AO8BJpVl","md5":"cf9ca42a3a9587d57993f1fb841758aa","sha1":"afda1fa59faabd6bacb17b1b17d8d647248a6c5c","sha256":"7de7222ee899f15874a1215940cf362079924c3e7154db4f904fb11a1c521312"},"dob":"1971-10-14 13:42:00","registered":"2015-03-17 08:33:02","phone":"(165)-662-2133","cell":"(481)-371-1537","id":{"name":"BSN","value":"20571705"}
+				   }]
  		})
         break;
     case 1:
@@ -53,13 +54,6 @@ app.get('/json', function(request, response) {
 		}]
  		})
         break;
-    case 5:
-        response.json({
-			"results":[{"gender":"female","name":{"title":"miss","first":"maudi","last":"den uijl"},"location":{"street":"4550 jan pieterszoon coenstraat","city":"goeree-overflakkee","state":"drenthe","postcode":86022},"email":"maudi.denuijl@example.com","login":{"username":"yellowlion527","password":"smiles","salt":"AO8BJpVl","md5":"cf9ca42a3a9587d57993f1fb841758aa","sha1":"afda1fa59faabd6bacb17b1b17d8d647248a6c5c","sha256":"7de7222ee899f15874a1215940cf362079924c3e7154db4f904fb11a1c521312"},"dob":"1971-10-14 13:42:00","registered":"2015-03-17 08:33:02","phone":"(165)-662-2133","cell":"(481)-371-1537","id":{"name":"BSN","value":"20571705"}
-				   }]
- 		})
-        break;
-	}
 })
 app.all('*', function(request, response) {
  response.status(404);
